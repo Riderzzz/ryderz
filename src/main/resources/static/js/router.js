acccbe00991bd1dffe12d967959619a71abe40c1
Irvin Ruiz <ruizirvin084@gmail.com>
2022-05-06 15:10:43 -0500

    added discover page, sigle event page, single group page and added to view controller

--- src/main/resources/static/js/router.js
+++ src/main/resources/static/js/router.js
@@ -13,6 +13,9 @@ import User from "./views/User.js";
 import logOut from "./views/logOut.js";
 import {LogOutEvent} from "./views/logOut.js";
 import NewsFeed from "./views/NewsFeed.js";
+import Discover from "./views/Discover.js";
+import Event from "./views/Event.js";
+import Group from "./views/Group.js";
 /**
  * Returns the route object for a specific route based on the given URI
  * @param URI
@@ -88,7 +91,23 @@ export default function router(URI) {
 			uri: '/newsfeed',
 			state: {},
 			title: 'News Feed'
-		}
+		},
+        '/discover': {
+            returnView: Discover,
+            uri: '/discover',
+            state: {},
+            title: 'Discover'
+        },
+        '/event': {
+            returnView: Event,
+            uri: '/event',
+            state: {},
+        },
+        '/group': {
+            returnView: Group,
+            uri: '/group',
+            state: {},
+        }
     };
 
     return routes[URI];
---
+++ src/main/resources/static/js/views/Discover.js
@@ -0,0 +1,13 @@
+export default function Discover(props) {
+	return `<!DOCTYPE html>
+<html lang="html">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Discover</title>
+    </head>
+    <body>
+    <h1>Discover</h1>
+    </body>
+</html>`;
+
+}
