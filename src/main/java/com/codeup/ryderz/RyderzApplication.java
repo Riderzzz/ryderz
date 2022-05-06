4c2eafdc18e8116260d39ed35ca9fc4cfc5ae20d
withers56 <william.k.withers@gmail.com>
2022-05-06 09:40:46 -0500

    first commit

---
+++ src/main/java/com/codeup/ryderz/RyderzApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.ryderz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyderzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RyderzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/ryderz/RyderzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.ryderz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyderzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
