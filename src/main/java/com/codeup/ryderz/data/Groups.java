b468631b1d4c50c7ad29b256bb0db630df255d32
withers56 <william.k.withers@gmail.com>
2022-05-06 12:29:47 -0500

    created groups class and updated user class

--- src/main/java/com/codeup/ryderz/data/Groups.java
+++ src/main/java/com/codeup/ryderz/data/Groups.java
@@ -1,5 +1,53 @@
 package com.codeup.ryderz.data;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+@Entity
+@Table(name = "groups")
 public class Groups {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String bio;
+
+    @Column
+    private LocalDate createDate;
+
+    @Column
+    private String location;
+
+    @ManyToOne
+    @JsonIgnoreProperties({"groups", "password"})
+    private User groupOwner;
+
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.DETACH, CascadeType.REFRESH},
+            targetEntity = Category.class)
+    @JoinTable(
+            name="group_users",
+            joinColumns = {@JoinColumn(name = "group_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name="user_id", nullable = false, updatable = false)},
+            foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT),
+            inverseForeignKey = @ForeignKey(ConstraintMode.CONSTRAINT)
+    )
+    @JsonIgnoreProperties("groups")
+    private List<User> users;
 }
