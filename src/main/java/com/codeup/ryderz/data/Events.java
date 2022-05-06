aabfbed50bd448f302d3caaf9a9c976274ec24a0
Dficci <danielficci@gmail.com>
2022-05-06 12:30:43 -0500

    added events

--- src/main/java/com/codeup/ryderz/data/Events.java
+++ src/main/java/com/codeup/ryderz/data/Events.java
@@ -1,8 +1,40 @@
 package com.codeup.ryderz.data;
 
-public class Events {
-    private Long id;
-    private Double longitude;
-    private Double latitude;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 
+public class Events {
+    @Id
+    private Long id;
+    @Column(nullable = false)
+    private Double startingLongitude;
+    @Column(nullable = false)
+    private Double startingLatitude;
+    @Column(nullable = false)
+    private Double endingLongitude;
+    @Column(nullable = false)
+    private Double endingLatitude;
+    @Column(nullable = false)
+    private Date createdDate;
+    @Column(nullable = false)
+    private Date eventDate;
+    @Column(nullable = false)
+    private String titleOfEvent;
+    @Column(nullable = false)
+    private String descriptionOfEvent;
+    @Column(nullable = false)
+    private String stateWhereEventTakesPlace;
+    @Column(nullable = false)
+    private String eventLocation;
+
+    @PrePersist
+    void setCreatedDate() {
+        this.createdDate  = new Date();
+    }
+
+    @ManyToOne
+    private User eventUserId;
 }
