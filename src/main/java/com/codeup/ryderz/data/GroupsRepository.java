7956fbaca58c3559a6f3768edf40b28fe7360c2e
withers56 <william.k.withers@gmail.com>
2022-05-06 14:17:25 -0500

    added backend functionality for getting all groups and creating a group

--- src/main/java/com/codeup/ryderz/data/GroupsRepository.java
+++ src/main/java/com/codeup/ryderz/data/GroupsRepository.java
@@ -1,4 +1,6 @@
 package com.codeup.ryderz.data;
 
-public interface GroupsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupsRepository extends JpaRepository<Groups, Long> {
 }
