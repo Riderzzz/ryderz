99ca674ef001482e1ee4e2d5822222e6f81fbf42
Dficci <danielficci@gmail.com>
2022-05-12 10:24:19 -0500

    working on being able to view other users profile

--- src/main/resources/static/js/views/Profile.js
+++ src/main/resources/static/js/views/Profile.js
@@ -2,6 +2,56 @@ import createView from "../createView.js";
 
 export default function Profile(props){
 console.log(props)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // **DO THIS**:
+//   Replace BUCKET_NAME with the bucket name.
+//
+    var albumBucketName = 'rydzerzcollection';
+
+// **DO THIS**:
+//   Replace this block of code with the sample code located at:
+//   Cognito -- Manage Identity Pools -- [identity_pool_name] -- Sample Code -- JavaScript
+//
+// Initialize the Amazon Cognito credentials provider
+    AWS.config.region = 'us-east-2'; // Region
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: 'us-east-2:466d1735-6b07-4cd6-b7da-1009316263c8',
+    });
+
+// Create a new service object
+    var s3 = new AWS.S3({
+        apiVersion: '2006-03-01',
+        params: {Bucket: albumBucketName}
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // language=HTML
     return `<!DOCTYPE html>
     <html lang="html">
