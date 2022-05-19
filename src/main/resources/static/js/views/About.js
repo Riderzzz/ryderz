db3cf56b93551b91d0d8b584597b7c878851c5da
Hector Mejia <mejia7hector@gmail.com>
2022-05-19 16:39:12 -0500

    added structure

--- src/main/resources/static/js/views/About.js
+++ src/main/resources/static/js/views/About.js
@@ -1,12 +1,23 @@
 export default function About(props) {
     return `
-    <div class="main-card d-flex">
-        <div class="our-story-card">
-            <div id="capstone-card" >
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At laudantium libero soluta. Aliquam amet at atque consectetur dolore dolorum earum excepturi facere, harum nesciunt non officia repudiandae sed unde ut!
+    <div class="main-card">
+        <div class="our-story-card d-flex">
+            <div id="capstone-card">
+                <h2>Ryderz Capstone</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam aperiam, architecto atque consectetur culpa cumque eaque, earum hic laboriosam nobis odio pariatur perferendis qui, quisquam sapiente sit tempore temporibus.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi fugiat, impedit laudantium quibusdam quidem quisquam tempore. Alias aliquam delectus fugit laudantium soluta? Accusantium debitis dicta recusandae sed sit temporibus tenetur.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad consequatur culpa delectus dicta dolorum ea et eveniet illo itaque, laborum, numquam quis recusandae sapiente tempore tenetur! Et eum quae sapiente?</p>
+                <p>loremdcasdcd</p>
+            </div>
+            </div>
+            <div id="week-1">
+            week 1
+            </div>
+            <div id="week-2">
+            week 1
             </div>
         </div>
-    </div>
+    
 <!--../../images/password-icon.png-->
     `;
 }
