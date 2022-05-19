9d66be3906a6da782c56c9a38b43f5174432c611
Hector Mejia <mejia7hector@gmail.com>
2022-05-19 15:20:23 -0500

    more-about.js flex

--- src/main/resources/static/js/views/About.js
+++ src/main/resources/static/js/views/About.js
@@ -1,12 +1,9 @@
 export default function About(props) {
     return `
-    <div class="our-story-card">
-        <div id="capstone-card" >
-            <h1 class="capstone-title">
-                Our Capstone
-            </h1> 
-            <div id="our-story">   
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam at consectetur, culpa dolore earum eligendi error explicabo magni molestias nemo neque non pariatur placeat, provident quia ratione sequi sunt totam.
+    <div class="main-card d-flex">
+        <div class="our-story-card">
+            <div id="capstone-card" >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At laudantium libero soluta. Aliquam amet at atque consectetur dolore dolorum earum excepturi facere, harum nesciunt non officia repudiandae sed unde ut!
             </div>
         </div>
     </div>
