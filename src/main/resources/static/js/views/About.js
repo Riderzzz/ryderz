5e6f48f897572bae3a47e22fdb63f3afa3e6909a
Hector Mejia <mejia7hector@gmail.com>
2022-05-25 17:49:58 -0500

    added to capstone story in -about.js

--- src/main/resources/static/js/views/About.js
+++ src/main/resources/static/js/views/About.js
@@ -2,9 +2,12 @@
 export default function About(props) {
     return `
     <div class="main-card container first-card">
+        <div class="h1 story__name block_text story-title--desktop">
+        Our development Process
+        </div>
         <div class="our-story-card d-flex row">
+            <h2>Ryderz Capstone</h2>
             <div id="capstone-card" class="col-6">
-                <h2>Ryderz Capstone</h2>
                 <p id="week-1-text">.</p>
                 <p id="week-2-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi fugiat, impedit laudantium quibusdam quidem quisquam tempore. Alias aliquam delectus fugit laudantium soluta? Accusantium debitis dicta recusandae sed sit temporibus tenetur.</p>
                 <p id="week-3-text"></p>
@@ -44,25 +47,25 @@ export default function About(props) {
             function() {
                 $('#capstone-card').html("Week 1 we used a skeleton of our rest blog to set up the foundation of our backend. Some of the methodology used includes SpringBoot for a relational database, javascript/jQuery, Oauth, ");
             }, function() {
-                $('#capstone-card').html("Given 4 weeks our group was tasked with creating our own application as our final project. After many hours of deliberation and the fact that we were the last group to decide. Our group decided to create a social media site for motorcycle riders looking looking to meet up for pre-planned routes.");
+                $('#capstone-card').html("Given 4 weeks our group was tasked with creating our very own programming design/implementation as our final project. After many hours of deliberation and the fact that we were the last group to decide. Our group decided to create a social media site for motorcycle riders looking looking to meet up for pre-planned routes.");
             }
         );$('#week-2').hover(
             function() {
                 $('#capstone-card').html("Week 2 most of the time was spent setting up group events allowing users to log into their own accounts to create public group ride events");
             }, function() {
-                $('#capstone-card').html("Given 4 weeks our group was tasked with creating our own application as our final project. After many hours of deliberation and the fact that we were the last group to decide. Our group decided to create a social media site for motorcycle riders looking looking to meet up for pre-planned routes.");
+                $('#capstone-card').html("Given 4 weeks our group was tasked with creating our very own programming design/implementation as our final project. After many hours of deliberation and the fact that we were the last group to decide. Our group decided to create a social media site for motorcycle riders looking looking to meet up for pre-planned routes.");
             }
         );$('#week-3').hover(
             function() {
                 $('#capstone-card').html("Week 3");
             }, function() {
-                $('#capstone-card').html("Given 4 weeks our group was tasked with creating our own application as our final project. After many hours of deliberation and the fact that we were the last group to decide. Our group decided to create a social media site for motorcycle riders looking looking to meet up for pre-planned routes.");
+                $('#capstone-card').html("Given 4 weeks our group was tasked with creating very own programming design/implementation as our final project. After many hours of deliberation and the fact that we were the last group to decide. Our group decided to create a social media site for motorcycle riders looking looking to meet up for pre-planned routes.");
             }
         );$('#week-4').hover(
             function() {
                 $('#capstone-card').html("Week 4");
             }, function() {
-                $('#capstone-card').html("Given 4 weeks our group was tasked with creating our own application as our final project. After many hours of deliberation and the fact that we were the last group to decide. Our group decided to create a social media site for motorcycle riders looking looking to meet up for pre-planned routes.");
+                $('#capstone-card').html("Given 4 weeks our group was tasked with creating very own programming design/implementation as our final project. After many hours of deliberation and the fact that we were the last group to decide. Our group decided to create a social media site for motorcycle riders looking looking to meet up for pre-planned routes.");
             }
         );
     }
