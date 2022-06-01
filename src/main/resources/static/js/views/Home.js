4d933ea1e2fa0b0cac6a2e2436f7641df6426539
Hector Mejia <mejia7hector@gmail.com>
2022-06-01 10:44:06 -0500

    added features to home

--- src/main/resources/static/js/views/Home.js
+++ src/main/resources/static/js/views/Home.js
@@ -20,6 +20,34 @@ export default function Home(props) {
             
             </div> 
         </main>
+        <div class="features-main d-flex ">
+            <h2 class="features-title justify-content-center">Features</h2>
+            <div class="features-card justify-content-space-around">
+                <div class="features">
+                Host/Join Clubs
+                </div>
+                <div class="features">
+                Create/View Motorcycle Routes
+                </div>
+                <div class="features">
+                Add/Message Friends
+                </div>
+            </div>
+        </div>
+        <div class="features-main d-flex ">
+            <h2 class="features-title justify-content-center">Features</h2>
+            <div class="features-card justify-content-space-around">
+                <div class="features">
+                Host/Join Clubs
+                </div>
+                <div class="features">
+                Create/View Motorcycle Routes
+                </div>
+                <div class="features">
+                Add/Message Friends
+                </div>
+            </div>
+        </div>
     `;
 }
 
