23510059b9a02f79d407e93f4ed06ea9653b4dda
Dficci <danielficci@gmail.com>
2022-06-06 10:10:15 -0500

    finished css for profile

--- src/main/resources/static/js/views/Register.js
+++ src/main/resources/static/js/views/Register.js
@@ -16,10 +16,6 @@ export default function Register(props) {
                                 <div class="card-body create-account-form">
                                     <h2 class="text-uppercase text-center mb-5">Create an account</h2>
                                     <form>
-                                        <!--                                        <div class="form-outline mb-4">-->
-                                        <!--                                            <input type="text" id="form3Example1cg"  class="form-control form-control-lg"/>-->
-                                        <!--                                            <label class="form-label" for="form3Example1cg">Your Name</label>-->
-                                        <!--                                        </div>-->
                                         
                                         <!-- username input -->
                                         <div class="form-outline mb-4 username-input-card">
