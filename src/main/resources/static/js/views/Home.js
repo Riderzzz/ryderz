fe998f22cbde91e4484e288389cc7ee692b56574
Hector Mejia <mejia7hector@gmail.com>
2022-06-07 12:28:57 -0500

    reformatted home

--- src/main/resources/static/js/views/Home.js
+++ src/main/resources/static/js/views/Home.js
@@ -4,50 +4,63 @@ export default function Home(props) {
 
     return `
         <main>
-        
-            <div class="pic-large d-none d-lg-block">
-                <div class="row">
-                    <div class="col-7"></div>
-                    <div class="col-5 mt-4">
-                        <p>Come join our community of like-minded riders and go on an adventure today!</p>
-                        <div>
-                            ${loginAndRegister()}
-                        </div>    
-                    </div> 
-                </div>            
-            </div>
-            <div class="pic-small d-lg-none">
-            
+
+    <div class="pic-large d-none d-lg-block">
+        <div class="row">
+            <div class="col-7"></div>
+            <div class="col-5 mt-4">
+                <p>Come join our community of like-minded riders and go on an adventure today!</p>
+                <div>
+                    ${loginAndRegister()}
+                </div>    
             </div> 
-        </main>
-        <div class="features-main d-flex ">
-            <h2 class="features-title justify-content-center">Features</h2>
-            <div class="features-card justify-content-space-around">
-                <div class="features">
-                Host/Join Clubs
-                </div>
-                <div class="features">
-                Create/View Motorcycle Routes
-                </div>
-                <div class="features">
-                Add/Message Friends
-                </div>
-            </div>
+        </div>            
+    </div>
+    <div class="pic-small d-lg-none">
+    
+    </div> 
+</main>
+<div class="features-main">
+    <h2 class="features-title d-flex justify-content-center">Features</h2>
+</div>
+<div class="features-card">
+    <div class="features">
+        <div>   
+            <i class="bi bi-calendar-date" style="font-size: 4rem;"></i>
         </div>
-        <div class="features-main d-flex ">
-            <h2 class="features-title justify-content-center">Features</h2>
-            <div class="features-card justify-content-space-around">
-                <div class="features">
-                Host/Join Clubs
-                </div>
-                <div class="features">
-                Create/View Motorcycle Routes
-                </div>
-                <div class="features">
-                Add/Message Friends
-                </div>
-            </div>
+        <div>
+            Host/Join Clubs
         </div>
+        <p class="first-features"> 
+            Host or join events with other motorcylists
+        </p>
+        <p class="second-features">
+         Choose your date and time!
+        </p>
+    </div>
+    <div class="features">
+        <div>
+            <i class="bi bi-pin-map" style="font-size: 4rem;"></i>
+        </div>
+        <div>
+            Create/View Motorcycle Routes
+        </div>
+        <p class="first-features"> 
+            Using google's map api, users can set markers on a map to show route start locations
+        </p>
+        <p> 
+        Find motorcycle riding events nearby!
+        </p>
+    </div>
+    <div class="features">
+        <div>
+            <i class="bi bi-person-plus" style="font-size: 4rem;"></i>
+        </div>
+        <div>
+            Add/Message Friends
+        </div>
+    </div>
+</div>
     `;
 }
 
