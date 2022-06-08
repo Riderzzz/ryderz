19d9761be9468a11bec91b23709b454b908a78e1
Hector Mejia <mejia7hector@gmail.com>
2022-06-08 00:19:34 -0500

    added_about_final

--- src/main/resources/static/js/views/About.js
+++ src/main/resources/static/js/views/About.js
@@ -111,7 +111,7 @@ export default function About(props) {
             <h2 class="about-names">Ryderz Capstone</h2>
             <div id="capstone-card" class="col-6">
                 <div>
-                Given 4 weeks our group was tasked with creating very own programming design/implementation as our final project. After many hours of deliberation and the fact that we were the last group to decide. Our group decided to create a social media site for motorcycle riders looking to meet up for pre-planned routes.
+                Given 4 weeks our group was tasked with creating our very own programming design/implementation as our final project. After many hours of deliberation and the fact that we were the last group to decide. Our group decided to create a social media site for motorcycle riders looking to meet up for pre-planned routes.
                 </div>
             </div>
             <ul class="col-6 d-flex row weeks-card">
@@ -133,13 +133,13 @@ export default function About(props) {
                     Week 1 we used a skeleton of our rest blog to set up the foundation of our backend. Some of the methodology used includes SpringBoot for a relational database, javascript/jQuery, Oauth security.
                 </p>
                 <p> 
-                    Week 2 most of the time was spent setting up group events allowing users to log into their own accounts to create public group ride events
+                    Week 2 most of the time was spent setting up group events allowing users to log into their own accounts to create public group ride events. Another great addition was the ability to upload photos using amazons s3 services and direct message functionality using pubnub's chat api.
                 </p>
                 <p> 
-                    Week 3
+                    Week 3 saw the group finish implementing many great features. Some of these include friend requests, data validation, club planning, discover page extras, google map api utilization, and sleek css.
                 </p>
                 <p> 
-                    Week 4
+                    Week 4 the main focus was finishing up more data validation and completing the final look of the website. A stronger focus on mobile views and overall user interface was out into our CSS/Js.
                 </p>
             </div>
         </div>
@@ -157,21 +157,21 @@ export function capstoneText() {
     );
     $('#week-2').hover(
         function () {
-            $('#capstone-card').html("Week 2 most of the time was spent setting up group events allowing users to log into their own accounts to create public group ride events");
+            $('#capstone-card').html("Week 2 most of the time was spent setting up group events allowing users to log into their own accounts to create public group ride events. Another great addition was the ability to upload photos using amazons s3 services and direct message functionality using pubnub's chat api");
         }, function () {
             $('#capstone-card').html("Given 4 weeks our group was tasked with creating our very own programming design/implementation as our final project. After many hours of deliberation and the fact that we were the last group to decide. Our group decided to create a social media site for motorcycle riders looking to meet up for pre-planned routes.");
         }
     );
     $('#week-3').hover(
         function () {
-            $('#capstone-card').html("Week 3");
+            $('#capstone-card').html("Week 3 saw the group finish implementing many great features. Some of these include friend requests, data validation, club planning, discover page extras, google map api utilization, and sleek css.");
         }, function () {
             $('#capstone-card').html("Given 4 weeks our group was tasked with creating very own programming design/implementation as our final project. After many hours of deliberation and the fact that we were the last group to decide. Our group decided to create a social media site for motorcycle riders looking to meet up for pre-planned routes.");
         }
     );
     $('#week-4').hover(
         function () {
-            $('#capstone-card').html("Week 4");
+            $('#capstone-card').html("Week 4 the main focus was finishing up more data validation and completing the final look of the website. A stronger focus on mobile views and overall user interface was out into our CSS/Js.");
         }, function () {
             $('#capstone-card').html("Given 4 weeks our group was tasked with creating very own programming design/implementation as our final project. After many hours of deliberation and the fact that we were the last group to decide. Our group decided to create a social media site for motorcycle riders looking to meet up for pre-planned routes.");
         }
